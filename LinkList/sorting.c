66e8c256c8656372605860779882218029ac0778
Arsalanbashir831 <69983716+Arsalanbashir831@users.noreply.github.com>
2021-09-25 14:52:30 +0500

    DSA

---
+++ LinkList/sorting.c
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void taversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d   ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *sort(struct Node *head)
+{
+    struct Node *p = head;
+    struct Node *q = p->next;
+    while (p->next != NULL)
+    {
+        q = p->next;
+        while (q != NULL)
+        {
+            if (p->data > q->data)
+            {
+                int temp;
+                temp = q->data;
+                q->data = p->data;
+                p->data = temp;
+            }
+            q = q->next;
+        }
+        p = p->next;
+    }
+    return head;
+}
+int main()
+{
+
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 46;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 48;
+    third->next = fourth;
+
+    fourth->data = 10;
+    fourth->next = NULL;
+    head = sort(head);
+    taversal(head);
+    return 0;
+}
---
+++ LinkList/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+struct Node *sort(struct Node *head){
+//     struct Node *ptr=head;
+//     struct Node *q=ptr->next;
+//     while (ptr!=NULL)
+//     {
+//         int temp;
+//         if (ptr->data>q->data)
+//         {
+//             temp=q->data;
+//             q->data=ptr->data;
+//             ptr->data=temp;
+//         }
+//         ptr=ptr->next;
+//         q=q->next;
+//     }
+//     return head;
+
+// }
