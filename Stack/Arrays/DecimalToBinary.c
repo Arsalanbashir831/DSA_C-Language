96c6ae2c72d59d6d878a5294f6ce1699c9895d03
Arsalanbashir831 <69983716+Arsalanbashir831@users.noreply.github.com>
2021-10-07 02:37:55 +0500

    Need correction

--- Stack/Arrays/DecimalToBinary.c
+++ Stack/Arrays/DecimalToBinary.c
@@ -1,67 +1,90 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-struct stack
+
+struct Stack
 {
     int size;
-    int *arr;
     int top;
+    int *arr;
 };
-struct stack *ptr;
-int push(int data)
-{
-    if (ptr->top == ptr->size - 1)
+
+void display(struct Stack *str){
+    for (int i = 0; i <= str->top; i++)
     {
-        return 0;
+        
+             printf("%d  ",str->arr[i]);
+    }
+    
+}
+int isEmpty(struct Stack *str)
+{
+    if (str->top == -1)
+    {
+        return 1;
     }
     else
     {
-        ptr->top++;
-        ptr->arr[ptr->top] = data;
+        return 0;
     }
 }
-int pop(){
-    int val;
-    if (ptr->top==-1)
+int isFull(struct Stack *str)
+{
+    if (str->top == str->size - 1)
     {
-        return 0;
+        return 1;
     }
     else
     {
-        val=ptr->arr[ptr->top];
-        ptr->top--;
+        return 0;
+    }
+}
+void push(struct Stack *str,int data){
+    if (isFull(str))
+    {
+        printf("Stack is full");
+    }
+    else{
+        str->top++;
+        str->arr[str->top]=data;
+    }
+}
+int pop(struct Stack *str){
+    if (isEmpty(str))
+    {
+        printf("Stack is Empty");
+    }
+    else{
+        int val=str->arr[str->top];
+        str->top=str->top-1;
+        return val;
+    }
+    
+}
+void decimalToBinary(int value,struct Stack *str){
+    str->top=-1;
+    int rem=0;
+    while (value!=0)
+    {
+        rem=value%2;
+        push(str,rem);
+        value/2;
+      
     }
 
-}
-int decimalToBinary(int data){
-    int rem;
-    while (data !=0)
-    {
-        rem = data%2;
-        push(data);
-        data/2;
-    }
-    int result;
-    while (ptr->top!=-1)
-    {
-       result=pop();
-    }
-    return result;
+    while(str->top!=-1)
+   {
+       printf("%d",pop(str));
+   }
+   
 }
 int main()
 {
- ptr = (struct stack *) malloc(sizeof(struct stack));
-    ptr->size = 100;
-    ptr->top = -1;
-    ptr->arr = (int *)malloc(ptr->size * sizeof(int));
-int integer=10;
-decimalToBinary(integer);
-int i=0;
-while (i<=ptr->arr[ptr->top])
-{
-    printf("%d ",ptr->arr[i]);
-    i++;
-}
-
+  struct Stack* str = (struct Stack *) malloc(sizeof(struct Stack));
+    str->size = 100;
+    str->top = -1;
+    str->arr=(int *)malloc(str->size*sizeof(int));
+   decimalToBinary(10,str);
+   
+    
     return 0;
 }
